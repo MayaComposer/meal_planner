d8cad336032eb3eb0c044a7cc77ed822b8d069f3
Maya Engel <mayaengelmusic@gmail.com>
2024-11-29 12:12:08 +0100

    code cleanup

--- meal_planner.py
+++ meal_planner.py
@@ -48,24 +48,74 @@ def generate_mealplan() -> list:
 
     np.random.shuffle(meals)
 
-    print('mealplan generated')
+    #print('mealplan generated')
 
     return meals
 
+shopping_list_string = ''
+shopping_list = []
+
 def display_mealplan():
     meals = generate_mealplan()
-    mealplan_text = ""
+    mealplan_text = ''
+
+    global shopping_list
+    global shopping_list_string
+    shopping_list = []
+    shopping_list_string = ''
+
     for count, meal in enumerate(meals):
-        mealplan_text += f"{days[count]}: \n{meal.Index} ({meal.category})\n"
-        mealplan_text += f"Ingredients: {meal.ingredients} {meal.fresh_ingredients}\n\n"
+        #meal plan
+        mealplan_text += f'{days[count]}: \n{meal.Index} ({meal.category})\n'
+        mealplan_text += f'Ingredients: {meal.ingredients} {meal.fresh_ingredients}\n\n'
+
+        #shopping list
+        shopping_list_string += f'{meal.ingredients}{meal.fresh_ingredients}'
+        shopping_list_string = shopping_list_string.replace(" ", "")
+        shopping_list = shopping_list_string.rsplit(',')
     
     mealplan_label.configure(text=mealplan_text)
 
+    shopping_list = remove_duplicates(shopping_list)
+
+    #configure shopping list
+    shopping_list_text = ''
+    
+    for ingredient in shopping_list:
+        shopping_list_text += f'- {ingredient}\n'
+
+    # print(shopping_list_string)
+    # print(shopping_list)
+    # print(shopping_list_text)
+
+    #also add to file
+    write_to_file(mealplan_text)
+
+def write_to_file(mealplan_text):
     with open("output.txt", "w") as f:
         f.write(mealplan_text)
 
 def add_recipe():
     print('here a recipe would be added')
+
+def remove_duplicates(list: list):
+
+    # A set to keep track of elements that have been seen
+    seen = set()
+    # A list to store duplicates found in the input list
+    duplicates = []
+
+    # Iterate over each element in the list
+    for i in list:
+        if i in seen:
+            duplicates.append(i)
+        else:
+            seen.add(i)
+
+    print(duplicates)
+    print(list)
+    return list
+
 
 # Set up customtkinter window
 root = ctk.CTk()
