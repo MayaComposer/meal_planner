a448182199f20456b45d680ef4b40a5217350b5a
Maya Engel <mayaengelmusic@gmail.com>
2024-12-02 14:45:45 +0100

    cleaned up code, added function to add recipe to dataframe

--- meal_planner_functions.py
+++ meal_planner_functions.py
@@ -24,6 +24,8 @@ def sort_recipes():
             veg_recipes.append(row)
     return fish_recipes, meat_recipes, veg_recipes
 
+
+
 def generate_mealplan() -> list:
     meals = []
 
@@ -53,7 +55,7 @@ def write_to_file(text: str) -> None:
 def add_recipe() -> None:
     global df, fish_recipes, meat_recipes, veg_recipes
 
-    recipe = pd.DataFrame([['TEST_INGREDIENTS', 'v', 'TEST_FRESH_INGREDIENTS', '']], columns=['ingredients', 'category', 'fresh_ingredients', 'instructions'], index=['ADDED_RECIPE'])
+    recipe = pd.DataFrame([['potato, potato, potato', 'v', 'mayo', '']], columns=['ingredients', 'category', 'fresh_ingredients', 'instructions'], index=['new_recipe'])
 
     #add a new recipe to df
     df = pd.concat([df, recipe])
@@ -78,6 +80,7 @@ def configure_shopping_list() -> str:
     for ingredient in shopping_list:
         shopping_list_text += f'- {ingredient}\n'
     return shopping_list_text
+
 
 def display_mealplan() -> None:
     meals = generate_mealplan()
@@ -109,42 +112,4 @@ def display_mealplan() -> None:
 
     #also add to file
     write_to_file(mealplan_text+shopping_list_text)
-
-
-#GLOBAL VARIABLES
-
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-# Read excel sheet into dataframe
-df = read_data()
-
-fish_recipes, meat_recipes, veg_recipes = sort_recipes()
-
-shopping_list_string = ''
-shopping_list = []
-
-# Set up customtkinter window
-root = ctk.CTk()
-root.title("Meal Planner")
-root.minsize(400, 400)  # width, height
-root.geometry("500x720+0+0")
-root.iconbitmap("icon.ico")
-
-root.grid_columnconfigure(0, weight=1)
-
-# Create button to generate meal plan
-generate_mealplan_button = ctk.CTkButton(root, text="Generate Meal Plan", command=display_mealplan, fg_color='teal', text_color='white')
-generate_mealplan_button.grid(row=0, column=0, padx=10, pady=10)
-
-add_recipe_button = ctk.CTkButton(root, text="Add new recipe", command=add_recipe, fg_color='teal', text_color='white')
-add_recipe_button.grid(row=1, column=0, padx=10, pady=10)
-
-quit_button = ctk.CTkButton(root, text='quit', command=quit, fg_color='teal', text_color='white')
-quit_button.grid(row=0, column=1, padx=0, pady=0)
-
-# Label to display the meal plan
-mealplan_label = ctk.CTkLabel(root, text="", justify="center", font=("Helvetica", 16))
-mealplan_label.grid(row=2, column=0, padx=10, pady=10, columnspan=2)
-
-root.mainloop()
 
